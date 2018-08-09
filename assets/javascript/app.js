cdffd163a25686fc12874d6b79a19561d46f97c2
Waleed Hamza <hamzah.waleed@gmail.com>
2018-08-09 13:02:34 -0400

    web complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
     $("#viewWork").click(function() {
         $('html,body').animate({
-            scrollTop: $("#portfolioItems").offset().top},
+            scrollTop: $("#portfolioDiv").offset().top},
             'slow');
     });
-});
 
+    $("#contactBtn").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".email").offset().top},
+            'slow');
+    });
+
+});
--- assets/javascript/slick.js
+++ assets/javascript/slick.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('.autoPlay').slick({
       dots: true,
+      arrows: true,
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 1,
@@ -12,21 +13,24 @@ $(document).ready(function(){
         breakpoint: 925,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          arrows: true
         }
       },
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          arrows: false
         }
       },
       {
         breakpoint: 500,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          arrows: false
         }
       }
     ]
