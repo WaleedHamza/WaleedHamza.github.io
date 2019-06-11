789031d7f667e704270f73631f010b633fb8c5e1
Waleed Hamza <hamzah.waleed@gmail.com>
2019-06-10 23:13:07 -0400

    +json

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { useRef, useEffect }  from 'react'
 import {Jumbotron, Container, Button} from 'react-bootstrap';
 import Background from '../particles/Particles';
 import './Home.css'
@@ -6,39 +6,60 @@ import './Home.css'
 // import { ReactComponent as Logo } from '../../Assets/mobile.svg';
 import About from '../about/About';
 import Portfolio from '../portfolio/Portfolio';
-export default class Home extends Component {
+import Contact from '../contact/Contact';
 
-    render() {
+
+const scrollToRef = (ref) => window.scrollTo(0, ref.ref)   
+const useMountEffect = (fun) => useEffect(fun, [])
+
+
+// class Home extends Component
+const Home = () =>
+ { 
+    const myRef = useRef(null)
+
+useMountEffect(() => scrollToRef(myRef)) // Scroll on mount
+
+    
         return (
             <div>
-            <div className='home'>
+            <div className='home'ref={myRef}>
                 <div className='particles'><Background />
                 </div>
                 <div className='hero'>
                     <Jumbotron fluid className='jumbotron'>
                         <Container fluid className='container'>
                             <h4>Hello, I am</h4>
-                            <h1 className='title'>WALEED HAMZA</h1>
+                            <h1 className='name'>WALEED HAMZA</h1>
                             <hr></hr>
                             <p className='text'>UX Designer / Frontend & Full Stack Developer</p>
                             <hr></hr>
-                            {/* <p className="lead ml2">HTML | CSS | JavaScript | JQuery
+                            <p >HTML | CSS | JavaScript | JQuery
                                 <br/>
                                 Node.js | Express | MongoDB | MYSQL | React.js
-                                </p> */}
+                                </p>
                                 <Button bsPrefix='aboutBtn' ><a className='btnLink' href="/about"> About Me</a></Button>
                         </Container>
                     </Jumbotron>
-                    {/* <Logo/> */}
-                    {/* <img className='photo' src={Image} alt={'Waleed'}></img> */}
-
                 </div>
 
             </div>
             <About />
             <Portfolio/>
+            <Contact/>
+
+            <button className='scrollToTopBtn' onClick={()=>scrollToRef(myRef)}>
+                    <i style={scrollBtn} className="fas fa-chevron-circle-up fa-3x"></i>
+                </button>
 
 </div>
         )
     }
-}
+
+
+
+const scrollBtn ={
+    color: '#00adee',
+  }
+
+export default Home;
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,22 +1,52 @@
 import React, {Component} from 'react'
 import {CardDeck, Card, Button} from 'react-bootstrap';
+// import {Animated} from "react-animated-css";
+import Slide from 'react-reveal/Slide';
+import Reveal from 'react-reveal/Reveal';
 import Projects from './projects.json';
 import './Portfolio.css';
 
-export default class Portfolio extends Component {
+class Portfolio extends Component {
+    constructor(props) {
+        super(props);
+        this.handleMouseHover = this.handleMouseHover.bind(this);
+        this.state = {
+          isHovering: false,
+        };
+      }
+    
+      handleMouseHover() {
+        this.setState(this.toggleHoverState);
+      }
+    
+      toggleHoverState(state) {
+        return {
+          isHovering: !state.isHovering,
+        };
+      }
+
     render() {
         return (
             <div className='portfolio'>
+                <Reveal effect="fadeInUp">
+                <h1 className='portfolioTitle'>Portfolio</h1></Reveal>
                 <CardDeck bsPrefix='deck'>
                     {Projects.map((item, index) => {
                         return (
-                  <Card className='card' key={item.id}>
-                  <Card.Img variant="top" src={require(`${item.img}`)} alt={item.name}></Card.Img>
-                  <Card.Body className='cardBody'>
-                  <Card.Title bsPrefix='cardTitle'>{item.name}</Card.Title>
-                  <Button className='cardBtn' variant="link"><a href={item.link}>Learn More</a></Button>
-                  </Card.Body>
-                  </Card>
+                            <Slide bottom>
+                                <Card className='card' key={item.id}>
+                                    <Card.Img variant="top" src={require(`${item.img}`)} alt={item.name} 
+                                      onMouseEnter={this.handleMouseHover}
+                                    onMouseLeave={this.handleMouseHover}></Card.Img>
+                                    {this.state.isHovering && <div className='overlay'>
+                                <Card.Body className='cardBody'>
+                                    <Card.Title bsPrefix='cardTitle'>{item.name}</Card.Title>
+                                    <Button className='cardBtn' variant="link">
+                                        <a href={item.link}>Learn More</a>
+                                    </Button>
+                                </Card.Body></div>}
+                                </Card>
+                                </Slide>
                         )
                     })}
                 </CardDeck>
@@ -24,3 +54,6 @@ export default class Portfolio extends Component {
         )
     }
 }
+
+
+export default Portfolio;
